84b548fe7c31eb4d901499c505d2d1ea81283439
SimonbJohnson <simonbjohnson@gmail.com>
2023-09-24 13:42:43 +0100

    adding set up file

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hapi-schema",
+    version="0.0.1",
+    author="HDX",
+    author_email="simon.johnson@un.org",
+    description="HAPI database schema specified in SQLAlchemy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OCHA-DAP/hapi-sqlalchemy-schema",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
