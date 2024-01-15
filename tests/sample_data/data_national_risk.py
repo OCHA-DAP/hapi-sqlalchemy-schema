1c23c728cb595c0af2d3d45e7460ff4569a26c45
Briar <briar.j.mills@gmail.com>
2024-01-15 11:40:54 -0500

    National Risk table and test

---
+++ tests/sample_data/data_national_risk.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+data_national_risk = [
+    dict(
+        id=1,
+        resource_ref=1,
+        location_ref=1,
+        risk_class=5,
+        global_rank=4,
+        overall_risk=8.1,
+        hazard_exposure_risk=8.7,
+        vulnerability_risk=8.5,
+        coping_capacity_risk=7.1,
+        meta_missing_indicators_pct=8,
+        meta_avg_recentness_years=0.26,
+        reference_period_start=datetime(2024, 1, 1),
+        reference_period_end=datetime(2024, 12, 31),
+        source_data="DATA,DATA,DATA",
+    ),
+]
---
+++ tests/test_national_risk_view.py
@@ -0,0 +1,21 @@
+from hdx.database.views import build_view
+
+from hapi_schema.db_national_risk import view_params_national_risk
+
+
+def test_national_risk_view(run_view_test):
+    """Check that national risk references other tables."""
+    view_national_risk = build_view(
+        view_params_national_risk.__dict__
+    )
+    run_view_test(
+        view=view_national_risk,
+        whereclause=(
+            view_national_risk.c.id == 1,
+            view_national_risk.c.dataset_hdx_id
+            == "c3f001fa-b45b-464c-9460-1ca79fd39b40",
+            view_national_risk.c.resource_hdx_id
+            == "90deb235-1bf5-4bae-b231-3393222c2d01",
+            view_national_risk.c.location_code == "FOO",
+        ),
+    )
