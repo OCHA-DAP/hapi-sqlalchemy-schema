373e56539680739bd92a445b27644bcabeb37b39
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-04-17 15:47:12 +0100

    HDX-9731 Fix tests failing because of over-enthusiastic use of unique constraints

--- tests/sample_data/data_patches.py
+++ tests/sample_data/data_patches.py
@@ -6,8 +6,8 @@ data_patches = [
         patch_sequence_number=1,
         commit_hash="66e7e589a1224a1832ba7360817dda7a7d9313cf",
         commit_date=datetime(2023, 1, 1),
-        patch_path="",
-        permanent_download_url="",
+        patch_path="2024/01",
+        permanent_download_url="https://github.com/OCHA-DAP/hapi-patch-repo/blob/66e7e589a1224a1832ba7360817dda7a7d9313cf/2024/01/hapi_patch_10_hno.json",
         state="executed",
     ),
     dict(
@@ -15,8 +15,8 @@ data_patches = [
         patch_sequence_number=2,
         commit_hash="554f18a92cf6a23a14e0f29356a6dec150f651ff",
         commit_date=datetime(2023, 1, 2),
-        patch_path="",
-        permanent_download_url="",
+        patch_path="2024/01",
+        permanent_download_url="https://github.com/OCHA-DAP/hapi-patch-repo/blob/554f18a92cf6a23a14e0f29356a6dec150f651ff/2024/01/hapi_patch_10_hno.json",
         state="failed",
     ),
     dict(
@@ -24,8 +24,8 @@ data_patches = [
         patch_sequence_number=3,
         commit_hash="35ea27da009e5add8d8d227e02fd2c4bbcc84b76",
         commit_date=datetime(2023, 1, 3),
-        patch_path="",
-        permanent_download_url="",
+        patch_path="2024/01",
+        permanent_download_url="https://github.com/OCHA-DAP/hapi-patch-repo/blob/35ea27da009e5add8d8d227e02fd2c4bbcc84b76/2024/01/hapi_patch_10_hno.json",
         state="discovered",
     ),
 ]
