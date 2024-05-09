0a36382c1379b002ffcd303456f68ac351a8bebe
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-05-09 07:49:47 +0100

    HDX-9781 Move the view as table code generator out of tests and into utils

--- src/hapi_schema/utils/view_as_table_code_generator.py
+++ src/hapi_schema/utils/view_as_table_code_generator.py
@@ -1,4 +1,21 @@
-import pytest
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+This script is designed to print a sqlalchemy table class from a view.
+To use it the code needs to be edited appropriately before running it:
+
+1. Update the import of the view_params below to pick the right view
+2. Change the target_view string to provide the new table name - conventionally this ends "_vat"
+3. Fill in the expected_primary_keys and expected_indexes lists
+4. Run the script - the class definition is output to console
+5. Copy the class definition to the file from which the view_params were
+6. Create a test of the view following the style, of test_operational_presence.py/test_operational_presence_vat -
+   this requires the expected_primary_keys and expected_indexes created in step 3 and will check the "view_as_tables"
+   columns match the view columns as well as checking the primary_keys and indexes.
+
+Ian Hopkinson 2024-05-09
+"""
+
 from hdx.database import Database
 from sqlalchemy import Table
 
@@ -7,11 +24,11 @@ from hapi_schema.db_national_risk import (
     view_params_national_risk,
 )
 from hapi_schema.utils.base import Base
+from hapi_schema.views import prepare_hapi_views
 
 
-@pytest.mark.skip(reason="This is not a real test")
-def test_output_table_code_to_stdout(session):
-    # Change these two
+def output_table_code_to_stdout():
+    # Change these the target_view, prepare_view, expected_primary_keys and expected_indexes
     target_view = "national_risk_view"
     _ = Database.prepare_view(view_params_national_risk.__dict__)
     expected_primary_keys = [
@@ -21,6 +38,8 @@ def test_output_table_code_to_stdout(session):
     ]
 
     expected_indexes = []
+    #
+    session = make_session()
     target_table = target_view.replace("view", "vat")
     Base.metadata.create_all(session.get_bind())
     Base.metadata.reflect(bind=session.get_bind(), views=True)
@@ -37,23 +56,27 @@ def test_output_table_code_to_stdout(session):
 
     Base.metadata.create_all(session.get_bind())
 
-    print(Base.metadata.tables.keys(), flush=True)
-
     assert target_table in Base.metadata.tables.keys()
-    assert False
 
 
 def make_table_template_from_view(
     target_table, columns, expected_indexes, primary_keys=["id"]
 ):
-    print(f"class DB{target_table}(Base):", flush=True)
-    print(f"\t__tablename__ = '{target_table}'", flush=True)
+    # Make a CamelCase name from the supplied table name
+    class_name = (
+        "DB"
+        + target_table.replace("_vat", "")
+        .replace("_", " ")
+        .title()
+        .replace(" ", "")
+        + "_vat"
+    )
+    print(f"class {class_name}(Base):", flush=True)
+    print(f"    __tablename__ = '{target_table}'", flush=True)
+
     new_columns = []
     for column in columns:
-        new_column = column.copy()
-        if column.name == "id":
-            column.primary_key = True
-
+        new_column = column._copy()
         primary_key_str = ""
         index_str = ""
         if column.name in primary_keys:
@@ -82,8 +105,21 @@ def make_table_template_from_view(
             mapped_type_1 = "str"
             mapped_type_2 = "Text"
         print(
-            f"\t{column.name}: Mapped[{mapped_type_1}] = mapped_column({mapped_type_2}{primary_key_str}{index_str})"
+            f"    {column.name}: Mapped[{mapped_type_1}] = mapped_column({mapped_type_2}{primary_key_str}{index_str})"
         )
 
         new_columns.append(new_column)
     return new_columns
+
+
+def make_session():
+    db_uri = "postgresql+psycopg://postgres:postgres@localhost:5432/hapitest"
+    database = Database(
+        db_uri=db_uri, recreate_schema=True, prepare_fn=prepare_hapi_views
+    )
+    session = database.get_session()
+    return session
+
+
+if __name__ == "__main__":
+    output_table_code_to_stdout()
