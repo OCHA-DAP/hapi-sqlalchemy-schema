be1afd20743767504bcc88435c32f63744a6005c
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-05-10 16:10:19 +0100

    HDX-9781 Update admin1_vat using new toml based scheme

--- src/hapi_schema/db_admin1.py
+++ src/hapi_schema/db_admin1.py
@@ -70,8 +70,8 @@ class DBAdmin1VAT(Base):
     __tablename__ = "admin1_vat"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     location_ref: Mapped[int] = mapped_column(Integer)
-    code: Mapped[str] = mapped_column(String(128))
-    name: Mapped[str] = mapped_column(String(512))
+    code: Mapped[str] = mapped_column(String(128), index=True)
+    name: Mapped[str] = mapped_column(String(512), index=True)
     is_unspecified: Mapped[bool] = mapped_column(Boolean)
     reference_period_start: Mapped[datetime] = mapped_column(
         DateTime, index=True
--- tests/test_admin1.py
+++ tests/test_admin1.py
@@ -17,16 +17,20 @@ def test_admin1_view(run_view_test):
     )
 
 
-def test_admin1_vat(run_indexes_test, run_columns_test):
+def test_admin1_vat(run_indexes_test, run_columns_test, run_primary_keys_test):
     """Check that the admin1 view as table is correct - columns match, expected indexes present"""
+    expected_primary_keys = ["id"]
     expected_indexes = [
-        "location_code",
-        "location_name",
+        "code",
+        "name",
         "reference_period_start",
         "reference_period_end",
+        "location_code",
+        "location_name",
     ]
     run_columns_test("admin1_vat", "admin1_view", view_params_admin1)
     run_indexes_test("admin1_vat", expected_indexes)
+    run_primary_keys_test("admin1_vat", expected_primary_keys)
 
 
 def test_reference_period_constraint(run_constraints_test):
