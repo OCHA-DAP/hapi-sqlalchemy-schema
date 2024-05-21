4a0c4e632babb3d5a93e509bb13503306a59871e
David Megginson <david.megginson@gmail.com>
2024-05-21 11:31:39 -0400

    Refactored poverty_rate table - use a text field for admin1_name - track rate instead of population - add rate_constraint - remove "*" from PovertyRateEnum

--- src/hapi_schema/db_poverty_rate.py
+++ src/hapi_schema/db_poverty_rate.py
@@ -1,22 +1,22 @@
 """PovertyRate table and view."""
 
 from datetime import datetime
+from decimal import Decimal
 
 from sqlalchemy import (
     DateTime,
     Enum,
     ForeignKey,
-    Integer,
+    String,
     select,
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from hapi_schema.db_admin1 import DBAdmin1
 from hapi_schema.db_location import DBLocation
 from hapi_schema.db_resource import DBResource
 from hapi_schema.utils.base import Base
 from hapi_schema.utils.constraints import (
-    population_constraint,
+    rate_constraint,
     reference_period_constraint,
 )
 from hapi_schema.utils.enums import PovertyClassification
@@ -27,7 +27,7 @@ from hapi_schema.utils.view_params import ViewParams
 class DBPovertyRate(Base):
     __tablename__ = "poverty_rate"
     __table_args__ = (
-        population_constraint(),
+        rate_constraint(),
         reference_period_constraint(),
     )
 
@@ -35,15 +35,18 @@ class DBPovertyRate(Base):
         ForeignKey("resource.hdx_id", onupdate="CASCADE", ondelete="CASCADE"),
         nullable=False,
     )
-    admin1_ref: Mapped[int] = mapped_column(
-        ForeignKey("admin1.id", onupdate="CASCADE"),
+    location_ref: Mapped[int] = mapped_column(
+        ForeignKey("location.id", onupdate="CASCADE"),
         nullable=False,
         primary_key=True,
+    )
+    admin1_name: Mapped[str] = mapped_column(
+        String(512), nullable=False, index=True, primary_key=True
     )
     classification: Mapped[PovertyClassification] = mapped_column(
         Enum(PovertyClassification), nullable=False, primary_key=True
     )
-    population: Mapped[int] = mapped_column(Integer, nullable=True, index=True)
+    rate: Mapped[Decimal] = mapped_column(nullable=True, index=False)
     reference_period_start: Mapped[datetime] = mapped_column(
         DateTime, primary_key=True
     )
@@ -52,7 +55,7 @@ class DBPovertyRate(Base):
     )
 
     resource = relationship(DBResource)
-    admin1 = relationship(DBAdmin1)
+    location = relationship(DBLocation)
 
 
 # view
@@ -63,18 +66,10 @@ view_params_poverty_rate = ViewParams(
         *DBPovertyRate.__table__.columns,
         DBLocation.code.label("location_code"),
         DBLocation.name.label("location_name"),
-        DBAdmin1.code.label("admin1_code"),
-        DBAdmin1.name.label("admin1_name"),
-        DBAdmin1.is_unspecified.label("admin1_is_unspecified"),
-        DBAdmin1.location_ref.label("location_ref"),
     ).select_from(
         DBPovertyRate.__table__.join(
-            DBAdmin1.__table__,
-            DBPovertyRate.admin1_ref == DBAdmin1.id,
-            isouter=True,
-        ).join(
             DBLocation.__table__,
-            DBAdmin1.location_ref == DBLocation.id,
+            DBPovertyRate.location_ref == DBLocation.id,
             isouter=True,
         )
     ),
--- tests/sample_data/data_poverty_rate.py
+++ tests/sample_data/data_poverty_rate.py
@@ -3,9 +3,10 @@ from datetime import datetime
 data_poverty_rate = [
     dict(
         resource_hdx_id="90deb235-1bf5-4bae-b231-3393222c2d01",
-        admin1_ref=2,
+        location_ref=1,
+        admin1_name="Province 01",  # not p-coded/normalised yet
         classification="vulnerable",
-        population=1000000,
+        rate=0.75,
         reference_period_start=datetime(2024, 1, 1),
         reference_period_end=datetime(2024, 12, 31),
     ),
