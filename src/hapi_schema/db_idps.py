f451440d2a58b5771ad0a6dfa7b5edb76f3ea793
Mike <mcarans@yahoo.co.uk>
2024-10-24 09:04:11 +1300

    HDXDSYS-1190 HAPI SQLAlchemy Schema - fix datetimes (#70)
    * Fix datetime columns
    
    * Update changelog before merge

--- src/hapi_schema/db_idps.py
+++ src/hapi_schema/db_idps.py
@@ -3,7 +3,6 @@
 from datetime import datetime
 
 from sqlalchemy import (
-    DateTime,
     ForeignKey,
     Integer,
     String,
@@ -57,13 +56,9 @@ class DBIDPs(Base):
     population: Mapped[int] = mapped_column(
         Integer, nullable=False, index=True
     )
-
-    reference_period_start: Mapped[datetime] = mapped_column(
-        DateTime, primary_key=True
-    )
-
+    reference_period_start: Mapped[datetime] = mapped_column(primary_key=True)
     reference_period_end: Mapped[datetime] = mapped_column(
-        DateTime, nullable=True, index=True
+        nullable=False, index=True
     )
 
     resource = relationship(DBResource)
